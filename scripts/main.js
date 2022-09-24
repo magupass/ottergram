03bb6959fa5bd6775b98eb6543ebcb8780201938
dev1 <dev1@dev.com>
2022-09-24 20:18:23 +0900

    ch7 end

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -27,10 +30,11 @@ function setDetailsFromThumb(thumbnail) {
 }
 
 function addThumbClickHandler(thumb) {
-    thumb.addEventListener('click', function (event) {
+    thumb.addEventListener('click', function(event) {
         console.log('aaa');
-      event.preventDefault();  
-      setDetailsFromThumb(thumb);
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails();
     });
 }
 
@@ -45,16 +49,29 @@ function hideDetails() {
     document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+function showDetails() {
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function() {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
 function addKeyPressHandler() {
-    document.body.addEventListener('keyup', function (event) {
+    document.body.addEventListener('keyup', function(event) {
         event.preventDefault();
-        console.log(event.keyCode);
+        console.log(event.key);
+        if (event.key == "Escape") {
+            hideDetails();
+        }
     })
 }
 
 function initializeEvents() {
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
 }
 
 initializeEvents();
